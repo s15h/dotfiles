edd583aed0da727868bac2f328f7f71718465f72
Sietze van den Bergh <sietze@s15h.nl>
2025-11-11 22:51:41 +0100

    Add discord install

--- initial-startup/debian/install_discord.sh
+++ initial-startup/debian/install_discord.sh
@@ -1,3 +1,5 @@
-# install discord
-wget -O discord.deb https://discord.com/api/download?platform=linux&format=deb
-sudo dpkg -i discord.deb
+# install discord if it is not installed
+if ! dpkg -l | grep discord; then
+  wget -O discord.deb https://discord.com/api/download?platform=linux&format=deb
+  sudo dpkg -i discord.deb
+fi
