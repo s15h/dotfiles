398f4da7ebf1f90a80e4d595aa9e8fcec306bced
Sietze van den Bergh <sietze@s15h.nl>
2025-11-13 18:02:41 +0100

    Add Zed

--- initial-startup/generic/install_all.sh
+++ initial-startup/generic/install_all.sh
@@ -33,6 +33,13 @@ else
   echo "Obsidian already installed."
 fi
 
+# Install zed
+if [ ! -f /usr/bin/zed ]; then
+  curl -f https://zed.dev/install.sh | sh
+else
+  echo "Zed already installed."
+fi
+
 # install oh my zsh
 if [ ! -d ~/.oh-my-zsh ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
