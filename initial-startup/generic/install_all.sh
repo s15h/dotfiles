fa5f1f7dc39e8a9c705d492f3f15f821a88fb08b
Sietze van den Bergh <sietze@s15h.nl>
2026-02-03 19:36:38 +0100

    Autoconfirm flatpak installs

--- initial-startup/generic/install_all.sh
+++ initial-startup/generic/install_all.sh
@@ -1,34 +1,34 @@
 # install bitwarden cli
 if [ ! -f /usr/bin/bw ]; then
-  flatpak install flathub com.bitwarden.desktop
+  flatpak install -y flathub com.bitwarden.desktop
 else
   echo "Bitwarden CLI already installed."
 fi
 
 # install bruno
 if [ ! -f /usr/bin/bruno ]; then
-  flatpak install flathub com.usebruno.Bruno
+  flatpak install -y flathub com.usebruno.Bruno
 else
   echo "Bruno already installed."
 fi
 
 # install discord
 if [ ! -f /usr/bin/discord ]; then
-  flatpak install flathub com.discordapp.Discord
+  flatpak install -y flathub com.discordapp.Discord
 else
   echo "Discord already installed."
 fi
 
 # install firefox
 if [ ! -f /usr/bin/firefox ]; then
-  flatpak install flathub org.mozilla.Firefox
+  flatpak install -y flathub org.mozilla.Firefox
 else
   echo "Firefox already installed."
 fi
 
 #install obsidian flatpack
 if [ ! -f /usr/bin/obsidian ]; then
-  flatpak install flathub md.obsidian.Obsidian
+  flatpak install -y flathub md.obsidian.Obsidian
 else
   echo "Obsidian already installed."
 fi
