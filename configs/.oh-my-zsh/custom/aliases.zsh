f4c075e4b929b200190d219b8282600c33f74569
Sietze van den Bergh <sietze@s15h.nl>
2026-02-14 15:00:25 +0100

    Chore: Add user and group to composer alias to avoid permission issues

--- configs/.oh-my-zsh/custom/aliases.zsh
+++ configs/.oh-my-zsh/custom/aliases.zsh
@@ -1,5 +1,5 @@
 alias yubikey-switch='gpg-connect-agent "scd serialno" "learn --force" /bye'
-alias composer='docker run --rm --interactive --tty --volume $PWD:/app --volume ${COMPOSER_HOME:-$HOME/.composer}:/tmp composer'
+alias composer='docker run --rm --interactive --tty --user "$(id -u):$(id -g)" --volume $PWD:/app --volume ${COMPOSER_HOME:-$HOME/.composer}:/tmp composer'
 alias docker-compose='docker compose'
 alias ec2-list-all='aws ec2 describe-instances --query '\''Reservations[].Instances[].[PrivateIpAddress,PrivateDnsName,InstanceId,Tags[?Key==`Name`].Value[]]'\'' --output text | sed '\''$!N;s/\n/ /'\'''
 alias bw="flatpak run --command=bw com.bitwarden.desktop"
@@ -11,4 +11,3 @@ alias goland-ssh='SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket) goland &
 alias pycharm-ssh='SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket) pycharm &'
 alias webstorm-ssh='SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket) webstorm &'
 alias intellij-ssh='SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket) idea &'
-
